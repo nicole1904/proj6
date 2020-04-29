b295685cdc3910e678ca02d4355f963b87a65fbe
nicole1904 <62022506+nicole1904@users.noreply.github.com>
2020-04-29 14:08:28 +0200

    Add files via upload
    GUI

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+import PySimpleGUI as sg
 
+class Application:
+
+    def run(self):
+        layout = [
+            [sg.Text("Enter user_id: "), sg.InputText(), sg.Button('OK')],
+            [sg.MLine(size=(60, 10), key="-OUTPUT-")],
+        ]
+        self.window = sg.Window("Recommender", layout)
+
+
+        while True:
+            event, values = self.window.read()
+            if event is None:
+                break
+
+            if event == "OK":
+                pass
+                # something something
+
+
+        self.window.close()
+
+
+def main():
+    Application().run()
+
+
+if __name__ == '__main__':
+    main()
+else:
+    print("ERROR: Try running from main")
